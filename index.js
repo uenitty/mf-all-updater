a2066f228b9a486bb021b64150841baeea7de552
uenitty <uenitty@gmail.com>
2024-04-21 11:05:32 +0900

    wrap try catch finally

--- index.js
+++ index.js
@@ -25,35 +25,35 @@ const SKIP_LIST = process.env.SKIP_LIST?.split(",") || [];
 
   const page = await context.newPage();
 
-  console.debug("goto /accounts");
-  await page.goto("/accounts");
-
-  const maintenanceText = page.getByText("メンテナンス作業中です");
-  if (await maintenanceText.count()) {
-    console.debug("exit メンテナンス作業中");
-    return;
-  }
-
-  const loginLink = page.locator('a[href="/sign_in"]', {
-    hasText: "ログイン",
-  });
-  if (await loginLink.count()) {
-    console.debug("click ログイン");
-    await Promise.all([page.waitForURL(/\/sign_in/), loginLink.click()]);
-  }
-
-  const loginWithEmailLink = page.locator('a[href^="/sign_in/email"]', {
-    hasText: "メールアドレスでログイン",
-  });
-  if (await loginWithEmailLink.count()) {
-    console.debug("click メールアドレスでログイン");
-    await Promise.all([
-      page.waitForURL(/\/sign_in/),
-      loginWithEmailLink.click(),
-    ]);
-  }
-
   try {
+    console.debug("goto /accounts");
+    await page.goto("/accounts");
+
+    const maintenanceText = page.getByText("メンテナンス作業中です");
+    if (await maintenanceText.count()) {
+      console.debug("exit メンテナンス作業中");
+      return;
+    }
+
+    const loginLink = page.locator('a[href="/sign_in"]', {
+      hasText: "ログイン",
+    });
+    if (await loginLink.count()) {
+      console.debug("click ログイン");
+      await Promise.all([page.waitForURL(/\/sign_in/), loginLink.click()]);
+    }
+
+    const loginWithEmailLink = page.locator('a[href^="/sign_in/email"]', {
+      hasText: "メールアドレスでログイン",
+    });
+    if (await loginWithEmailLink.count()) {
+      console.debug("click メールアドレスでログイン");
+      await Promise.all([
+        page.waitForURL(/\/sign_in/),
+        loginWithEmailLink.click(),
+      ]);
+    }
+
     const emailInput = page.locator('input[type="email"]');
     const passwordInput = page.locator('input[type="password"]');
     console.debug("fill EMAIL");
@@ -65,52 +65,46 @@ const SKIP_LIST = process.env.SKIP_LIST?.split(",") || [];
       page.waitForURL(/\/sign_in/),
       passwordInput.press("Enter"),
     ]);
-  } catch (error) {
-    console.error(error);
-    console.error("page.content()\n-----\n", await page.content(), "\n-----");
-    throw error;
-  }
 
-  console.debug("goto /accounts");
-  await page.goto("/accounts");
+    console.debug("goto /accounts");
+    await page.goto("/accounts");
 
-  const useThisAccountButton = page.getByRole("button", {
-    name: "このアカウントを使用する",
-  });
-  if (await useThisAccountButton.count()) {
-    console.debug("click このアカウントを使用する");
-    await Promise.all([
-      page.waitForURL(/\/accounts/),
-      useThisAccountButton.click(),
-    ]);
-  }
-
-  const selectThisAccountButton = page.getByRole("button", {
-    name: "メールアドレスでログイン",
-  });
-  if (await selectThisAccountButton.count()) {
-    console.debug("click メールアドレスでログイン");
-    await Promise.all([
-      page.waitForURL(/\/accounts/),
-      selectThisAccountButton.click(),
-    ]);
-  }
-
-  const rows = page
-    .locator("section#registration-table.accounts table#account-table tr")
-    .filter({
-      has: page.getByRole("button", { name: "更新" }),
+    const useThisAccountButton = page.getByRole("button", {
+      name: "このアカウントを使用する",
     });
+    if (await useThisAccountButton.count()) {
+      console.debug("click このアカウントを使用する");
+      await Promise.all([
+        page.waitForURL(/\/accounts/),
+        useThisAccountButton.click(),
+      ]);
+    }
 
-  const accountNames = await Promise.all(
-    (await rows.locator("td:first-child a:first-child").all()).map(
-      (firstChild) => firstChild.textContent(),
-    ),
-  );
-  console.debug("accountNames", accountNames);
+    const selectThisAccountButton = page.getByRole("button", {
+      name: "メールアドレスでログイン",
+    });
+    if (await selectThisAccountButton.count()) {
+      console.debug("click メールアドレスでログイン");
+      await Promise.all([
+        page.waitForURL(/\/accounts/),
+        selectThisAccountButton.click(),
+      ]);
+    }
 
-  for (let accountName of accountNames) {
-    try {
+    const rows = page
+      .locator("section#registration-table.accounts table#account-table tr")
+      .filter({
+        has: page.getByRole("button", { name: "更新" }),
+      });
+
+    const accountNames = await Promise.all(
+      (await rows.locator("td:first-child a:first-child").all()).map(
+        (firstChild) => firstChild.textContent(),
+      ),
+    );
+    console.debug("accountNames", accountNames);
+
+    for (let accountName of accountNames) {
       if (SKIP_LIST.includes(accountName)) {
         console.info("skip", accountName);
         continue;
@@ -128,17 +122,17 @@ const SKIP_LIST = process.env.SKIP_LIST?.split(",") || [];
       }
       const action = await form.getAttribute("action");
       await Promise.all([page.waitForResponse(action), updateButton.click()]);
-    } catch (error) {
-      console.error(`caught error at ${accountName} row`);
-      console.error(error);
-      console.error("::group::page.content()");
-      console.error(await page.content());
-      console.error("::endgroup::");
-      throw error;
     }
-  }
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
 
-  console.debug("close browser");
-  await context.close();
-  await browser.close();
+    console.error("::group::page.content()");
+    console.error(await page.content());
+    console.error("::endgroup::");
+  } finally {
+    console.debug("close browser");
+    await context.close();
+    await browser.close();
+  }
 })();
