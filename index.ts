acd871dea21d3f5c8c5c572a0228c4bc0f2ca07f
Ueni <uenitty@gmail.com>
2024-08-28 11:00:44 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ const SKIP_LIST = process.env.SKIP_LIST?.split(",") || [];
       headless: HEADLESS === "true",
       locale: "ja-JP",
       userAgent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.48 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
     },
   );
 
