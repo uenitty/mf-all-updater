4d386cfff71714b5535d9fb1600f04bdccf9df95
uenitty <uenitty@gmail.com>
2025-02-13 23:02:04 +0900

    improve

--- update_manual_account.ts
+++ update_manual_account.ts
@@ -68,7 +68,7 @@ const PORTAL_PASSWORD = process.env.PORTAL_PASSWORD || "";
     await Promise.all([page.waitForURL(/\/membertop/), portalButton.click()]);
 
     console.debug("goto detail");
-    await page.getByRole("link", { name: "持株会" }).first().click();
+    await page.getByRole("link").getByText("持株会", { exact: true }).click();
     await page.getByRole("link", { name: "拠出状況照会" }).first().click();
     await page.locator("div.loading-div").waitFor({ state: "hidden" });
 
